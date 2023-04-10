1f3acd9e9a462d7343aa24366dc598b801ff8ec0
jhonvelasque <jhonvelasque@unmsm.edu.pe>
2023-04-10 09:48:21 -0500

    creando portqada de nuestra api

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
-
+from fastapi.responses import HTMLResponse
 app = FastAPI()
 
-
+@app.get('/')
+def home():
+    return HTMLResponse('<h1 style=color:blue> hola mundo </h1>')
 @app.get("/inicio")
 async def ruta_prueba():
     return "La pagina de jhon"
