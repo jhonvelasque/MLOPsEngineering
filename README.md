10151b1c1cf45ebfbf8a654b2a23b37393d297f2
jhonvelasque <jhonvelasque@unmsm.edu.pe>
2023-04-17 14:03:55 -0500

    terminacion del proyecto solo falta terminar el readme

--- README.md
+++ README.md
@@ -1,6 +1,28 @@
-# MLOPsEngineering
-en este proyectose desarollo Transformaciones,Desarrollo API,Deployment,Análisis exploratorio de los datos,Sistema de recomendación
-### 
+# PROYECTO INDIVIDUAL N°1 -  MLOPsEngineering
+## resumen 
+- En este proyectose desarollo Transformaciones,Desarrollo API,Deployment,Análisis exploratorio de los datos,Sistema de recomendación.
+## Problemática:
+- Simulación de un equipo de data empresarial donde el área de análisis de datos le solicita al de Data engineering que, utilizando un grupo de datasets provistos, realice las transformaciones requeridas y posteriormente ponga a disposición los datos mediante la elaboración y ejecución de una API
+## Rol
+- Data engineer 👩‍💻
+
+## requerimientos
+```bash
+  pip install requirements.txt
+```
+
+## Proceso
+### Proceso de "ETL" (Extract, transform, load) en VisualStudioCode - Python:
+- 
+- 
+### Desarrollo de las consultas solicitadas (API) :
+- 
+- 
+### Analisis exploratorio (EDA):
+
+
+### Implementacion Algoritmode recomendacion :
+
 videos en los que me apoye
 - https://www.youtube.com/watch?v=IBfj_0Zf2Mo
 - https://www.youtube.com/watch?v=w9kibRWlQQg
--- main.py
+++ main.py
@@ -25,6 +25,7 @@ def home():
     return HTMLResponse('<h1 style=color:blue>LA APP JHON </h1>')
 
 @app.get('/get_max_duration/{year}/{platform}/{duration_type}',tags=["movies"])
+#funcion nos permite hallar la pelicula con mayor duracion
 async def get_max_duration(year:int, platform:str ,duration_type:str):
     #haciendo los respectivos filtros
     df2=movie_df[(movie_df["release_year"]==year) & (movie_df["plataforma"]==platform) & (movie_df["duration_unit"]==duration_type)]
@@ -33,10 +34,12 @@ async def get_max_duration(year:int, platform:str ,duration_type:str):
     #hallando el nombre de la pelicula
     peli_max=df2.loc[index_max,"title"]
     return {'pelicula':peli_max}
+#cantidad de peliculas segun las plataformas
 
 @app.get('/get_score_count/{platform}/{scored}/{year}',tags=["movies"])
+#funcion nos trae el total de peliculas que tiene mayor score que el ingresado
 async def get_score_count(platform:str, scored:int, year:int):
-    df=movie_df[(movie_df["release_year"]==year) & (movie_df["plataforma"]==platform) & (movie_df["score"]>scored)]
+    df=movie_df[(movie_df["release_year"]==year) & (movie_df["plataforma"]==platform) & (movie_df["score"]>scored)&(movie_df["duration_unit"]=='min')]
     cantidad=df.shape[0]
     return {
         'plataforma': platform,
@@ -46,37 +49,42 @@ async def get_score_count(platform:str, scored:int, year:int):
     }
 
 @app.get('/get_count_platform/{platform}',tags=["movies"])
+#funcion que nos trae la cantidad de peluclas segun la plataforma
 async def get_count_platform(platform:str):
-    df=movie_df[(movie_df["plataforma"]==platform)]
+    df=movie_df[(movie_df["plataforma"]==platform)&(movie_df["duration_unit"]=='min')]
     count= df.shape[0]
     return {'plataforma': platform, 'peliculas': count}
 
 @app.get('/get_actor/{platform}/{year}',tags=["movies"])
+#funcion que nos trae el actor que mas aparece en una plataforma
 async def get_actor(platform:str, year:int):
     df=movie_df[(movie_df["plataforma"]==platform) & (movie_df["release_year"]==year)]
-    actor_masrepetido=df['director'].value_counts().sort_values(ascending=False).idxmax()
+    actor_masrepetido=df['cast'].str.split(',').explode().value_counts().sort_values(ascending=False).idxmax()
+    apariciones=int(df['cast'].str.split(',').explode().value_counts().sort_values(ascending=False)[0])
     return {
         'plataforma': platform,
         'anio': year,
         'actor': actor_masrepetido,
-        'apariciones': actor_masrepetido
+        'apariciones': apariciones
     }
-
+#la funcion  devuelve la cantidada de contenidos/productos segun el tipo de contenido
 @app.get('/prod_per_county/{tipo}/{pais}/{anio}',tags=["movies"])
 async def prod_per_county(tipo:str,pais:str,anio:int):
     df=movie_df[(movie_df["country"]==pais) & (movie_df["release_year"]==anio)& (movie_df["type"]==tipo)]
     cant_peliculas_serie=df['duration_unit'].value_counts()
-
+    cantidad=int(cant_peliculas_serie[1])
     return {'pais': pais,
              'anio': anio, 
-             'peliculas': cant_peliculas_serie[1]
+             'contenido': cantidad
             }
 
-@app.get('/get_contents/{rating}')
+@app.get('/get_contents/{rating}',tags=["movies"])
+#la funcion devuelve cantidad total de contenidos/productos
 async def get_contents(rating:int):
     df=movie_df[movie_df["score"]==10]
     return {'rating': rating, 'contenido': df.shape[0]}
 
+#aqui implementamos nuestro algoritmo de recomendacion 
 @app.get('/get_recomendation/{title}',tags=["movies"])
 async def get_recommendations(title:str):
     idx = movies_df.index[movies_df['title'] == title].tolist()[0]
